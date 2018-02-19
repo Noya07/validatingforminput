484b6749627c7ff2cdd20ea665d78511cb3f3689
Vilius Ramoska <vilius.ramoska@gmail.com>
2018-02-19 09:35:49 +0100

    initial commit

---
+++ src/main/java/example/PersonForm.java
@@ -0,0 +1,50 @@
+package example;
+
+import org.hibernate.validator.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class PersonForm {
+
+    @NotNull
+    @Size(min=2, max=30)
+    private String name;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @NotNull
+    @Min(18)
+    private Integer age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+        return "Person( Name: " + this.name + ", Age: " + this.age + ", Email: " + this.email + ")";
+    }
+}
